00f4a2f107ab814adef5adcb359beb3dfd3df193
Robert Hogan <robhogan@gmail.com>
2024-06-24 22:49:16 +0100

    Merge sound sample table together

--- src/images_sounds.s
+++ src/images_sounds.s
@@ -21,8 +21,39 @@
 .DC.L $0000
 .incbin "incbin/paddingbetweentunesandsmp.bin"
 
-.incbin "incbin/smp.bin"
-.incbin "incbin/paddingbetweensmpandsamples.bin"
+
+; dc.b 'Engine Noise 1      ', $0001, $01, $ac, $009acd00, $0011a000, $009acd02, $00119e00
+; dc.b 'Player Shot Normal 2', $0002, $01, $ac, $009adea4, $0008e800, $009adea4, $00000000
+; dc.b 'Engine Noise        ', $0003, $01, $ac, $009ae290, $00337800, $009aee9e, $00259400
+; dc.b 'Player Death        ', $0004, $00, $d6, $00000000, $00549a00, $00000000, $00000000
+; dc.b 'Player Death 2      ', $0005, $01, $ac, $00000000, $00245800, $00000000, $00000000
+; dc.b 'Player Shot Normal  ', $0006, $01, $ac, $009b160c, $0007a400, $009b160c, $00000000
+; dc.b 'Player Jump         ', $0007, $01, $ac, $009b1db4, $0018de00, $009b1db4, $00000000
+; dc.b 'Crackle             ', $0008, $00, $d6, $009b3696, $00459400, $009b3696, $00000000
+; dc.b 'Cleared Level       ', $0009, $01, $ac, $009b7c2e, $0037a200, $009b7c2e, $00000000
+; dc.b 'Warp                ', $000a, $02, $38, $009bb3d4, $006ec800, $009bb3d4, $00000000
+; dc.b 'Large Explosion     ', $000b, $01, $ac, $009c22a0, $0050c200, $009c22a0, $00000000
+; dc.b 'Powered Up Shot     ', $000c, $01, $ac, $009c7366, $00197600, $009c7366, $00000000
+; dc.b 'Get Power Up        ', $000d, $01, $ac, $009c8ce0, $001aea00, $009c8ce0, $00000000
+; dc.b 'Tink For Spike      ', $000e, $00, $fe, $009ca7ce, $00040e00, $009ca7ce, $00000000
+; dc.b 'NME At Top Of Web   ', $000f, $01, $ac, $009cabe0, $00001e00, $009cabe0, $00000000
+; dc.b 'Pulse For Pulsar    ', $0010, $03, $58, $009cac02, $0019fe00, $009cac02, $00000000
+; dc.b 'Normal Explosion    ', $0011, $00, $d6, $009cc604, $002ab600, $009cc604, $00000000
+; dc.b 'Extra Explosion     ', $0012, $03, $58, $009cf0be, $0018ca00, $009cf0be, $00000000
+; dc.b 'Static or Pulsar    ', $0013, $01, $1c, $009d098c, $003fe400, $009d098c, $00000000
+; dc.b 'Pulsar Pulse        ', $0014, $03, $58, $009d4974, $000f0c00, $009d4974, $00000000
+; dc.b 'Off Shielded NME    ', $0015, $00, $aa, $009d5884, $0027ca00, $009d5884, $00000000
+; dc.b 'Excellent           ', $0016, $02, $00, $009d8052, $00597600, $009d8052, $00000000
+; dc.b 'Superzapper Recharge', $0016, $02, $00, $009dd9cc, $00a95800, $009dd9cc, $00000000
+; dc.b 'yes                 ', $0018, $02, $00, $009e8328, $005a6c00, $009e832a, $005a6a00
+; dc.b 'oneup               ', $0019, $02, $00, $009edd98, $0043ae00, $009edd98, $00000000
+; dc.b 'screeeam            ', $001a, $02, $00, $009f214a, $00456800, $009f214a, $00000000
+; dc.b 'sexy yes 1          ', $001b, $02, $00, $009f66b6, $002c5400, $009f66b6, $00000000
+; dc.b 'sexy yes 2          ', $001c, $02, $00, $009f9362, $00323600, $009f9362, $00000000
+; dc.b 'tink                ', $001e, $02, $00, $009fc59c, $0005ce00, $009fc59c, $00000000
+; dc.b 'zero                ', $001f, $02, $00, $009fcb6e, $00000800, $009fcb6e, $00000000
+; dc.b 'dummy               ', $0020, $02, $00, $009fcb7a, $00a1d800, $009fcb7a, $00000000
+.incbin "incbin/sound_samples_table.bin"
 
 .incbin "sounds/samples/01"
 .DC.L $0000
